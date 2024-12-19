5b88af45558fe4bae4d53c08b2ca36ddcf77e24e
XMTKBRX <160078586+XMTKBRX@users.noreply.github.com>
2024-12-19 06:37:30 +0200

    Update بوتتتت.js

--- script/commands/بوتتتت.js
+++ script/commands/بوتتتت.js
@@ -1,26 +1,17 @@
 module.exports.config = {
-   name: "المطور",
-    version: "1.0.0",
-    hasPermssion: 0,
-    credits: "𝐢𝐤𝐚 𝐛𝐨𝐭",
-    description: "",
-    usages: "",
-    commandCategory: "العاب",
-    cooldowns: 0,
-  dependencies: {
-    "request":"",
-    "fs-extra":"",
-    "axios":""
-  }
+  name: "المطور",
+  version: "1.0.0",
+  hasPermision: 0,
+  credits: "𝐢𝐤𝐚 𝐛𝐨𝐭",
+  description: "يرسل لك نكتة عشوائية",
+  usage: "مطور",
+  commandCategory: "ترفية",
+  cooldowns: 0,
 };
 
-module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currencies}) => {
-const axios = global.nodemodule["axios"];
-const request = global.nodemodule["request"];
-const fs = global.nodemodule["fs-extra"];
-   var hi = ["𝐢𝐤𝐚 𝐛𝐨𝐭\n m.me/100078235290006"];
-  var know = hi[Math.floor(Math.random() * hi.length)];
-  var link = ["https://i.imgur.com/Oer7E6u.jpeg"]; 
-	      var callback = () => api.sendMessage({body:`「♡」 ${know} 「♡」`,attachment: fs.createReadStream(__dirname + "/cache/5.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/5.jpg"));	
-      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/5.jpg")).on("close",() => callback());
-   };
+module.exports.run = async function ({ api, event }) {
+  const jokes = [m.me/100078235290006];
+
+  const joke = jokes[Math.floor(Math.random() * jokes.length)];
+  api.sendMessage(joke, event.threadID, event.messageID);
+};
